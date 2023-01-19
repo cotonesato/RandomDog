a4813429b65f313503dd98d5ecee4eb05bf07c48
cotonesato <cocchanday.310@gmail.com>
2023-01-20 01:10:39 +0900

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import "bulma/css/bulma.css";
+
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+createRoot(document.querySelector("#content")).render(<App />);
+if(mouseClick ){
+    window.location.reload();
+}
+
+
